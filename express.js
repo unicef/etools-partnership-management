44c84ce982e14a81073b49173539f415131ec929
robertavram <robert.avram@gmail.com>
2017-02-01 11:45:25 -0500

    Fix: Routing for redux

--- express.js
+++ express.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var basedir = '/code/build/pmp/bundled/';
+var node_modulesDir = '/code/node_modules/';
 //var basedir = '/Users/Robi/Desktop/etools/infra/etools-infra/pmp/build/pmp/bundled/';
 
 
@@ -9,7 +10,7 @@ app.use('/pmp/', express.static(basedir));
 app.get(/.*service-worker\.js/, function(req, res) {
   res.sendFile(basedir + 'service-worker.js');
 });
-
+app.get(/node_modules/, express.static(node_modulesDir));
 app.use(function(req, res) {
   res.sendFile(basedir + 'index.html');
 });
