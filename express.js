d91ab1e2aa1f30b6cfd939f4eec11ab33c874602
robertavram <robert.avram@gmail.com>
2017-02-01 12:45:31 -0500

    Fix routing redo

--- express.js
+++ express.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
 var basedir = '/code/build/pmp/bundled/';
-var node_modulesDir = '/code/node_modules/';
-//var basedir = '/Users/Robi/Desktop/etools/infra/etools-infra/pmp/build/pmp/bundled/';
+//var basedir = '/Users/rob/Desktop/etools/etools-infra/pmp/build/pmp/bundled/';
+var node_modulesReduxDir = '/code/node_modules/redux/dist/';
+//var node_modulesReduxDir = '/Users/rob/Desktop/etools/etools-infra/pmp/node_modules/redux/dist/';
 
 
 app.use('/pmp/', express.static(basedir));
@@ -10,7 +11,9 @@ app.use('/pmp/', express.static(basedir));
 app.get(/.*service-worker\.js/, function(req, res) {
   res.sendFile(basedir + 'service-worker.js');
 });
-app.get(/node_modules/, express.static(node_modulesDir));
+app.get(/.*redux\.min\.js/, function(req, res) {
+  res.sendFile(node_modulesReduxDir + 'redux.min.js');
+});
 app.use(function(req, res) {
   res.sendFile(basedir + 'index.html');
 });
