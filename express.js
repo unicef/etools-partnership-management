09ade67fa86fb8e7371f21d9a1c58a91538f71c6
Adrian Hangan <adrian.hangan@nordlogic.com>
2018-02-13 15:07:30 +0200

    feature: Polymer 2 migration

--- express.js
+++ express.js
@@ -2,18 +2,18 @@ var express = require('express');
 var app = express();
 var basedir = __dirname + '/build/pmp/bundled/';
 //var basedir = '/Users/rob/Desktop/etools/etools-infra/pmp/build/pmp/bundled/';
-var node_modulesReduxDir = __dirname + '/node_modules/redux/dist/';
+// var node_modulesReduxDir = __dirname + '/node_modules/redux/dist/';
 //var node_modulesReduxDir = '/Users/rob/Desktop/etools/etools-infra/pmp/node_modules/redux/dist/';
-
 
 app.use('/pmp/', express.static(basedir));
 
 app.get(/.*service-worker\.js/, function(req, res) {
   res.sendFile(basedir + 'service-worker.js');
 });
-app.get(/.*redux\.min\.js/, function(req, res) {
-  res.sendFile(node_modulesReduxDir + 'redux.min.js');
-});
+
+// app.get(/.*redux\.min\.js/, function(req, res) {
+//   res.sendFile(node_modulesReduxDir + 'redux.min.js');
+// });
 
 app.use(function(req, res) {
   // static file requrests that end up here are missing so they should return 404
