80a62e623beed4f2aaa9796ddf74be098ca7bbbc
Adriana Trif <trif.corina.adriana@gmail.com>
2018-02-22 15:56:06 +0200

    Polymer2 - differential serving

--- express.js
+++ express.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var browserCapabilities = require('browser-capabilities');
+var prplServer = require('prpl-server');
 
 var app = express();
 var basedir = __dirname + '/build/';
@@ -8,25 +8,12 @@ var serveFromDir = basedir + 'es6-bundled/';
 // var node_modulesReduxDir = __dirname + '/node_modules/redux/dist/';
 //var node_modulesReduxDir = '/Users/rob/Desktop/etools/etools-infra/pmp/node_modules/redux/dist/';
 
-// TODO - use prpl-server
-var chooseBuild = function (request, response, next) {
-  let clientCapabilities = browserCapabilities.browserCapabilities(
-    request.headers['user-agent']);
-
-  clientCapabilities = new Set(clientCapabilities);
-
-  if (clientCapabilities.has("es2015")) {
-    serveFromDir = basedir + 'es6-bundled/';
-  } else {
-    serveFromDir = basedir + 'es5-bundled/';
-  }
-  console.log('dir', serveFromDir);
-  next();
-}
-
-
- app.use(chooseBuild);
- app.use('/pmp/', express.static(serveFromDir));
+ app.use('/pmp/', prplServer.makeHandler('build/pmp/', {
+  builds: [
+    {name: 'es6-bundled', browserCapabilities: ['es2015']},
+    {name: 'es5-bundled'}
+  ],
+}));
 
 // app.get(/.*service-worker\.js/, function(req, res) {
 //   res.sendFile(basedir + 'service-worker.js');
@@ -36,16 +23,16 @@ var chooseBuild = function (request, response, next) {
 //   res.sendFile(node_modulesReduxDir + 'redux.min.js');
 // });
 
-app.use(function(req, res) {
-  // static file requrests that end up here are missing so they should return 404
-  if (req.originalUrl.startsWith('/pmp/pmp/')) {
-    res.status(404).send('Not found');
-  } else {
-    console.log('build', serveFromDir);
-    // handles requests that look like /pmp/interventions/details
-    res.sendFile(serveFromDir + 'index.html');
-  }
-});
+// app.use(function(req, res) {
+//   // static file requrests that end up here are missing so they should return 404
+//   if (req.originalUrl.startsWith('/pmp/pmp/')) {
+//     res.status(404).send('Not found');
+//   } else {
+//     console.log('build', serveFromDir);
+//     // handles requests that look like /pmp/interventions/details
+//     res.sendFile(serveFromDir + 'index.html');
+//   }
+// });
 
 
 app.listen(8080);
