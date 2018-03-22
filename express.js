0d7d3d49150dab242ae6e28f5c5bf7b8c3f07df8
daniel tabarcea <dani.tabarcea@gmail.com>
2018-03-22 19:06:38 +0200

    fixed indentation

--- express.js
+++ express.js
@@ -5,21 +5,21 @@ var app = express();
 var basedir = __dirname + '/build/pmp/';
 
 function getSourcesPath (request){
-    let clientCapabilities = browserCapabilities.browserCapabilities(
-      request.headers['user-agent']);
+  let clientCapabilities = browserCapabilities.browserCapabilities(
+    request.headers['user-agent']);
 
-    clientCapabilities = new Set(clientCapabilities);
+  clientCapabilities = new Set(clientCapabilities);
 
-    if (clientCapabilities.has("es2015")) {
-      return basedir + 'es6-bundled/'
-    } else {
-      return basedir + 'es5-bundled/'
-    }
-  };
+  if (clientCapabilities.has("es2015")) {
+    return basedir + 'es6-bundled/'
+  } else {
+    return basedir + 'es5-bundled/'
+  }
+};
 
 app.use('/pmp/', (req, res, next) => {
-    express.static(getSourcesPath(req))(req, res, next)
-  });
+  express.static(getSourcesPath(req))(req, res, next)
+});
 
 app.get(/.*service-worker\.js/, function(req, res) {
   res.sendFile(getSourcesPath(req) + 'service-worker.js');
