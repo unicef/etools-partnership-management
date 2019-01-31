fa70daeaa4624235611bf1b1407df9fb2a2c0fae
Adriana Trif <trif.corina.adriana@gmail.com>
2019-01-31 20:00:06 +0200

    Handle partners data storage in redux

---
+++ src/reducers/partners.ts
@@ -0,0 +1,56 @@
+
+import {createSelector} from 'reselect';
+import * as a from '../actions/partners';
+import { isEmptyObject } from '../components/utils/utils';
+import { RootState } from '../store';
+
+export class PartnersState {
+  list: [] = [];
+}
+
+const INITIAL_STATE = new PartnersState();
+
+const partners = (state = INITIAL_STATE, action: any) => {
+  switch(action.type) {
+    case a.SET_PARTNERS:
+      return {
+        list: action.partners
+      }
+    default:
+      return state;
+  }
+}
+
+export default partners;
+
+// ------- Selectors ---
+
+const partnersSelector = (state: RootState) => state.partners!.list;
+const notHiddenPartnersSelector = createSelector(
+  partnersSelector,
+  (partners: any) => {
+    return partners.filter((p:any) => !p.hidden);
+  }
+);
+
+export const csoPartnersSelector = createSelector(
+  notHiddenPartnersSelector,
+  (partners: any) => {
+    return partners.filter((p:any) =>
+     p.partner_type === 'Civil Society Organization');
+  }
+);
+
+export const partnersForDropdownsSelector = createSelector(
+  notHiddenPartnersSelector,
+  (partners: any) => {
+    return partners.map((p: any) => {
+      return {
+        value: p.id,
+        label: p.name
+      };
+    });
+  }
+);
+
+
