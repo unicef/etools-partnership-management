d81f2fa3ff3a1e77dd3390390c63d7f132f2da71
Adrian Hangan <iovuadrian@gmail.com>
2019-03-05 13:56:39 +0200

    fix imports

--- dev-reinstall.sh
+++ dev-reinstall.sh
@@ -2,7 +2,8 @@
 
 # Removing etools components from node_modules will ensure we'll get the latest updates from git repos
 echo "Removing etools components installed with npm..."
-rm -rf ./node_modules/etools-*
+rm -rf ./node_modules
+rm -rf ./package-lock.json
 
 echo "Install/update node packages..."
 npm i
