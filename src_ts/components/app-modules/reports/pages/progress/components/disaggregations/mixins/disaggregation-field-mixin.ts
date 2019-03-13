b47ac41916477f4d801beac030baf9d59a4c221d
Adrian Hangan <iovuadrian@gmail.com>
2019-03-13 15:37:45 +0200

    reports ts lint issues fix

--- src_ts/components/app-modules/reports/pages/progress/components/disaggregations/mixins/disaggregation-field-mixin.ts
+++ src_ts/components/app-modules/reports/pages/progress/components/disaggregations/mixins/disaggregation-field-mixin.ts
@@ -6,7 +6,7 @@ import { dedupingMixin } from '@polymer/polymer/lib/utils/mixin';
  */
 const DisaggregationFieldMixin = dedupingMixin((superClass: any) => class extends superClass {
   _toNumericValues(obj: any) {
-    return Object.keys(obj).reduce(function(prev, curr) {
+    return Object.keys(obj).reduce(function(prev: any, curr: any) {
       prev[curr] = Number(obj[curr]);
 
       return prev;
