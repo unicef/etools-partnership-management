e5856529d3498a673d586a113dc553ef606b1cbc
Adrian Hangan <iovuadrian@gmail.com>
2019-03-15 15:39:21 +0200

    changed add/update cp dialog label

--- src_ts/components/app-modules/interventions/pages/details/components/results/result-cp-output-and-ram-indicators.ts
+++ src_ts/components/app-modules/interventions/pages/details/components/results/result-cp-output-and-ram-indicators.ts
@@ -42,7 +42,7 @@ class ResultCpOutputAndRamIndicators extends EtoolsMixinFactory.combineMixins([
 
     <etools-dialog id="cpOutputRamIndicatorsDialog"
                   size="md"
-                  dialog-title="Add/Update CP Indicators"
+                  dialog-title="Add/Update CP Output/CP Inidcators"
                   ok-btn-text="Add/Update"
                   disable-confirm-btn="[[disableConfirmBtn]]"
                   on-confirm-btn-clicked="_saveChanges"
