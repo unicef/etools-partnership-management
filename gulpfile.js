6556d2d7109795debadc9bf8a061ce3624ef8581
Adriana Trif <trif.corina.adriana@gmail.com>
2019-06-05 16:47:14 +0300

    Build with polymer-build library

--- gulpfile.js
+++ gulpfile.js
@@ -18,210 +18,15 @@ const del = require('del');
 const spawn = require('child_process').spawn;
 const polymerBuilds = require('./build_helpers/polymer-builds');
 const fileSystemHelper = require('./build_helpers/file-system-helper');
-const babel = require('gulp-babel');
+const buildWithPolymerLib = require('./build_helpers/polymer-build-library');
 
-
-
-//const del = require('del');
-//const gulp = require('gulp');
-const gulpif = require('gulp-if');
-const mergeStream = require('merge-stream');
-const polymerBuild = require('polymer-build');
-const through2 = require('through2').obj;
+//const through2 = require('through2').obj;
 //let uglify = require('gulp-uglify-es').default;
-let uglify = require('babel-preset-minify');
+//let uglify = require('babel-preset-minify');
 
-// Here we add tools that will be used to process our source files.
-//const imagemin = require('gulp-imagemin');
-
-// Additional plugins can be used to optimize your source files after splitting.
-// Before using each plugin, install with `npm i --save-dev <package-name>`
 // const uglify = require('gulp-uglify');
 // const cssSlam = require('css-slam').gulp;
 // const htmlMinifier = require('gulp-html-minifier');
-
-const swPrecacheConfig = require('./sw-precache-config.js');
-const polymerJson = require('./polymer.json');
-const polymerProject = new polymerBuild.PolymerProject(polymerJson);
-const buildDirectory = 'build';
-
-function matchesExt(extension) {
-  return (fs) => !!fs.path && fs.relative.endsWith(extension);
-}
-
-/**
- * Waits for the given ReadableStream
- */
-function waitFor(stream) {
-  return new Promise((resolve, reject) => {
-    stream.on('end', resolve);
-    stream.on('error', reject);
-  });
-}
-
-function pipeStreams(streams) {
-  return Array.prototype.concat.apply([], streams)
-      .reduce((a, b) => {
-        return a.pipe(b);
-      });
-}
-
-async function build(done) {
- // return new Promise((resolve, reject) => { // eslint-disable-line no-unused-vars
-
-    // Lets create some inline code splitters in case you need them later in your build.
-    //let sourcesStreamSplitter = new polymerBuild.HtmlSplitter();
-    //let dependenciesStreamSplitter = new polymerBuild.HtmlSplitter();
-
-    // Okay, so first thing we do is clear the build directory
-    //console.log(`Deleting ${buildDirectory} directory...`);
-    // del([buildDirectory])
-    //   .then(() => {
-
-        // Let's start by getting your source files. These are all the files
-        // in your `src/` directory, or those that match your polymer.json
-        // "sources"  property if you provided one.
-        //let sourcesStream = polymerProject.sources();
-
-          // If you want to optimize, minify, compile, or otherwise process
-          // any of your source code for production, you can do so here before
-          // merging your sources and dependencies together.
-         // .pipe(gulpif(/\.(png|gif|jpg|svg)$/, imagemin()))
-
-          // The `sourcesStreamSplitter` created above can be added here to
-          // pull any inline styles and scripts out of their HTML files and
-          // into seperate CSS and JS files in the build stream. Just be sure
-          // to rejoin those files with the `.rejoin()` method when you're done.
-         // .pipe(sourcesStreamSplitter.split())
-
-          // Uncomment these lines to add a few more example optimizations to your
-          // source files, but these are not included by default. For installation, see
-          // the require statements at the beginning.
-          // .pipe(gulpif(/\.js$/, uglify())) // Install gulp-uglify to use
-          // .pipe(gulpif(/\.css$/, cssSlam())) // Install css-slam to use
-          // .pipe(gulpif(/\.html$/, htmlMinifier())) // Install gulp-html-minifier to use
-
-          // Remember, you need to rejoin any split inline code when you're done.
-          //.pipe(sourcesStreamSplitter.rejoin());
-
-
-        // Similarly, you can get your dependencies seperately and perform
-        // any dependency-only optimizations here as well.
-        //let dependenciesStream = polymerProject.dependencies();
-         // .pipe(dependenciesStreamSplitter.split())
-          // Add any dependency optimizations here.
-         // .pipe(dependenciesStreamSplitter.rejoin());
-
-        //const htmlSplitter = new polymerBuild.HtmlSplitter();
-        // Okay, now let's merge your sources & dependencies together into a single build stream.
-        let buildStream = mergeStream(polymerProject.sources(), polymerProject.dependencies())
-          .once('data', () => {
-            console.log('Analyzing build dependencies...');
-          });
-
-        // If you want bundling, pass the stream to polymerProject.bundler.
-        // This will bundle dependencies into your fragments so you can lazy
-        // load them.
-        // ------ Bundle --------------
-        buildStream = buildStream.pipe(polymerProject.bundler({
-          stripComments: true
-        }));
-
-
-       // buildStream = buildStream.pipe(uglify({mangle: false}));
-        // buildStream = pipeStreams([
-        //   buildStream,
-        //   ...polymerBuild.getOptimizeStreams({js:{minify:true, moduleResolution: 'node'}})
-        //   ]);
-
-        buildStream = buildStream.pipe(gulp.dest(buildDirectory+'/esm-bundled'));
-
-
-        //----- Transform Modules to AMD ----------
-        // buildStream = buildStream.pipe(gulpif(matchesExt('.js'), babel({
-        //   plugins: ["@babel/plugin-transform-modules-amd"]
-        // })));
-        buildStream = pipeStreams([
-          buildStream,
-
-          ...polymerBuild.getOptimizeStreams({js: {
-                   transformModulesToAmd: true,
-                   moduleResolution: 'node',
-                   minify: true
-                 },
-                 entrypointPath: polymerProject.config.entrypoint
-               })
-        ]);
-
-        // ------ Minify Js ------------
-        // buildStream = buildStream.pipe(gulpif(matchesExt('.js'),babel({presets:[['minify', {
-        //   builtIns: false,
-        //   evaluate: false,
-        //   mangle: false,
-        // }]]})));
-
-        buildStream = buildStream.pipe(gulp.dest(buildDirectory+'/es6-bundled'))
-
-        // ---- Transpile to ES5 --------------
-
-        buildStream = buildStream.pipe(polymerProject.addCustomElementsEs5Adapter())
-        buildStream = pipeStreams([
-          buildStream,
-          ...polymerBuild.getOptimizeStreams({js: {
-                                             compile: true,
-                                             moduleResolution: 'node'
-                                           },
-                                           entrypointPath: polymerProject.config.entrypoint
-                                         })
-        ]);
-        // buildStream = buildStream.pipe(gulpif(matchesExt('.js'), babel({
-        //   plugins: [
-        //     '@babel/plugin-proposal-object-rest-spread',
-        //     '@babel/plugin-transform-template-literals',
-        //     '@babel/plugin-transform-literals',
-        //     '@babel/plugin-transform-function-name',
-        //     '@babel/plugin-transform-arrow-functions',
-        //     '@babel/plugin-transform-block-scoped-functions',
-        //     '@babel/plugin-transform-classes',
-        //     '@babel/plugin-transform-object-super',
-        //     '@babel/plugin-transform-shorthand-properties',
-        //     '@babel/plugin-transform-duplicate-keys',
-        //     '@babel/plugin-transform-computed-properties',
-        //     '@babel/plugin-transform-for-of',
-        //     '@babel/plugin-transform-sticky-regex',
-        //     '@babel/plugin-transform-unicode-regex',
-        //     '@babel/plugin-transform-spread',
-        //     '@babel/plugin-transform-parameters',
-        //     '@babel/plugin-transform-destructuring',
-        //     '@babel/plugin-transform-block-scoping',
-        //     '@babel/plugin-transform-typeof-symbol',
-        //     '@babel/plugin-transform-instanceof'
-        //   ]
-        // })));
-
-        buildStream.pipe(gulp.dest(buildDirectory+'/es5-bundled'));
-
-        // waitFor the buildStream to complete
-        await waitFor(buildStream);
-      //})
-     // .then(() => {
-        // Okay, now let's generate the Service Worker
-        console.log('Generating the Service Worker...');
-        await polymerBuild.addServiceWorker({
-          project: polymerProject,
-          buildRoot: buildDirectory,
-          bundled: true,
-          swPrecacheConfig: swPrecacheConfig
-        });
-      //})
-      //.then(() => {
-        // You did it!
-        console.log('Build complete!');
-        done();
-     // });
- // });
-}
-
 
 
 // ---------------------------------------------------------------------------------
@@ -287,7 +92,11 @@ gulp.task('moveTempToBuildFolder', (done) => {
   });
 });
 
-gulp.task('build-with-lib', (done) => build(done));
+gulp.task('build-with-lib', gulp.series(buildWithPolymerLib.deleteBuildDirectory,
+  gulp.parallel(buildWithPolymerLib.build, buildWithPolymerLib.generateServiceWorker),
+  buildWithPolymerLib.moveServiceWorker));
 
 gulp.task('build1by1', gulp.series(polymerBuilds.buildEsmBundled, polymerBuilds.buildEs6Bundled,
 polymerBuilds.buildEs5Bundled));
+
+
