12b3f1104014d53e0a5fc185b43d77620737a0d7
danNordlogic <dan.cioara@nordlogic.com>
2019-07-25 10:06:31 +0300

    [ch11545] Allow deletion of a PV visit row in Draft

--- src_ts/components/app-modules/interventions/pages/details/components/planned-visits.ts
+++ src_ts/components/app-modules/interventions/pages/details/components/planned-visits.ts
@@ -1,4 +1,4 @@
-import {PolymerElement, html} from '@polymer/polymer';
+import { PolymerElement, html } from '@polymer/polymer';
 import '@polymer/iron-icons/iron-icons.js';
 import '@polymer/iron-flex-layout/iron-flex-layout.js';
 import '@polymer/paper-icon-button/paper-icon-button.js';
@@ -8,16 +8,16 @@ import '@polymer/paper-button/paper-button.js';
 import '@unicef-polymer/etools-dropdown/etools-dropdown.js';
 import '../../../../../layout/etools-form-element-wrapper.js';
 import RepeatableDataSetsMixin from '../../../../../mixins/repeatable-data-sets-mixin';
-import {DomRepeatEvent, GenericObject} from '../../../../../../typings/globals.types';
-import {PlannedVisit} from '../../../../../../typings/intervention.types';
-import {gridLayoutStyles} from '../../../../../styles/grid-layout-styles.js';
-import {SharedStyles} from '../../../../../styles/shared-styles.js';
-import {repeatableDataSetsStyles} from '../../../../../styles/repeatable-data-sets-styles.js';
-import {buttonsStyles} from '../../../../../styles/buttons-styles.js';
-import {fireEvent} from '../../../../../utils/fire-custom-event.js';
-import {property} from '@polymer/decorators';
-import {EtoolsDropdownEl} from '@unicef-polymer/etools-dropdown/etools-dropdown.js';
-import {PaperInputElement} from '@polymer/paper-input/paper-input.js';
+import { DomRepeatEvent, GenericObject } from '../../../../../../typings/globals.types';
+import { PlannedVisit } from '../../../../../../typings/intervention.types';
+import { gridLayoutStyles } from '../../../../../styles/grid-layout-styles.js';
+import { SharedStyles } from '../../../../../styles/shared-styles.js';
+import { repeatableDataSetsStyles } from '../../../../../styles/repeatable-data-sets-styles.js';
+import { buttonsStyles } from '../../../../../styles/buttons-styles.js';
+import { fireEvent } from '../../../../../utils/fire-custom-event.js';
+import { property } from '@polymer/decorators';
+import { EtoolsDropdownEl } from '@unicef-polymer/etools-dropdown/etools-dropdown.js';
+import { PaperInputElement } from '@polymer/paper-input/paper-input.js';
 
 /**
  * @polymer
@@ -203,23 +203,23 @@ class PlannedVisits extends RepeatableDataSetsMixin(PolymerElement) {
     `;
   }
 
-  @property({type: String})
+  @property({ type: String })
   _deleteEpName: string = 'interventionPVLinkDelete';
 
-  @property({type: Array})
+  @property({ type: Array })
   years: [] = [];
 
   // @ts-ignore
-  @property({type: Boolean, observer: PlannedVisits.prototype._editModeChanged})
+  @property({ type: Boolean, observer: PlannedVisits.prototype._editModeChanged })
   editMode!: boolean;
 
-  @property({type: Number})
+  @property({ type: Number })
   interventionId!: number;
 
-  @property({type: String})
+  @property({ type: String })
   interventionStatus!: string;
 
-  @property({type: Object})
+  @property({ type: Object })
   extraEndPointParams!: GenericObject;
 
   static get observers() {
@@ -231,7 +231,6 @@ class PlannedVisits extends RepeatableDataSetsMixin(PolymerElement) {
   ready() {
     super.ready();
     this.dataSetModel = new PlannedVisit();
-    this.set('extraEndPointParams', {intervention_id: this.interventionId});
   }
 
   _getAddBtnPadding(itemsLength: number) {
@@ -281,7 +280,7 @@ class PlannedVisits extends RepeatableDataSetsMixin(PolymerElement) {
     const q3 = this.shadowRoot!.querySelector('#visit_' + index + '_q3') as PaperInputElement;
     const q4 = this.shadowRoot!.querySelector('#visit_' + index + '_q4') as PaperInputElement;
 
-    [q1, q2, q3, q4].forEach(function(q) {
+    [q1, q2, q3, q4].forEach(function (q) {
       if (q) {
         if (!q.validate()) {
           valid = false;
@@ -304,6 +303,7 @@ class PlannedVisits extends RepeatableDataSetsMixin(PolymerElement) {
     if (!Array.isArray(dataItems)) {
       this.set('dataItems', []);
     }
+    this.set('extraEndPointParams', { intervention_id: this.interventionId });
   }
 
   /**
@@ -311,7 +311,7 @@ class PlannedVisits extends RepeatableDataSetsMixin(PolymerElement) {
    */
   _canBeRemoved(index: number, editMode: boolean, interventionStatus: string) {
 
-    if(!editMode || interventionStatus !== 'draft'){
+    if (!editMode || !(interventionStatus === '' || interventionStatus === 'draft')) {
       return false;
     }
     if (!this.dataItems || !this.dataItems.length || !this.dataItems[index]) {
@@ -327,7 +327,7 @@ class PlannedVisits extends RepeatableDataSetsMixin(PolymerElement) {
     const yearDropdown = this.shadowRoot!.querySelector('#year_' + event.model.index);
 
     if (this.isAlreadySelected(yearSelected, event.model.index, 'year')) {
-      fireEvent(this, 'toast', {text: 'Year already selected on other planned visit item.', showCloseBtn: true});
+      fireEvent(this, 'toast', { text: 'Year already selected on other planned visit item.', showCloseBtn: true });
       this._clearSelectedYear(yearDropdown, event);
     }
   }
@@ -348,7 +348,7 @@ class PlannedVisits extends RepeatableDataSetsMixin(PolymerElement) {
    */
   _addNewPlannedVisit() {
     if (!this.validate()) {
-      fireEvent(this, 'toast', {text: 'Already added planned visit data is not valid yet', showCloseBtn: true});
+      fireEvent(this, 'toast', { text: 'Already added planned visit data is not valid yet', showCloseBtn: true });
       return;
     }
     this._addElement();
@@ -358,4 +358,4 @@ class PlannedVisits extends RepeatableDataSetsMixin(PolymerElement) {
 
 window.customElements.define('planned-visits', PlannedVisits);
 
-export {PlannedVisits as PlannedVisitsEl};
+export { PlannedVisits as PlannedVisitsEl };
