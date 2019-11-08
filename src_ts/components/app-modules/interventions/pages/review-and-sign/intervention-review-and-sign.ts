cdf6ef2f1eb969c5cf24b71751a4c8a9097be7de
dcioara <dan.cioara@nordlogic.com>
2019-11-08 13:39:03 +0200

    [ch16442] Status section jumps to the bottom of the page

--- src_ts/components/app-modules/interventions/pages/review-and-sign/intervention-review-and-sign.ts
+++ src_ts/components/app-modules/interventions/pages/review-and-sign/intervention-review-and-sign.ts
@@ -77,6 +77,11 @@ class InterventionReviewAndSign extends connect(store)(CommonMixin(
             opacity: 1;
           };
         }
+
+        .col_prc_doc {
+          max-width:400px;
+        }
+
       </style>
 
       <etools-content-panel class="content-section" panel-title="Signatures & Dates">
@@ -126,7 +131,7 @@ class InterventionReviewAndSign extends connect(store)(CommonMixin(
                                 selected-date-display-format="D MMM YYYY">
               </datepicker-lite>
             </div>
-            <div class="col col-6">
+            <div class="col col-6 col_prc_doc">
               <!-- PRC Review Document -->
               <etools-upload
                   label="PRC Review Document"
@@ -328,7 +333,7 @@ class InterventionReviewAndSign extends connect(store)(CommonMixin(
 
   _isDraft(status: string) {
     return status === CONSTANTS.STATUSES.Draft.toLowerCase() ||
-        status === '';
+      status === '';
   }
 
   _interventionChanged(intervention: Intervention) {
@@ -430,7 +435,7 @@ class InterventionReviewAndSign extends connect(store)(CommonMixin(
     }
     // this functionality is available only after pd is saved and in draft status
     if (this.intervention &&
-        this.intervention.status === CONSTANTS.STATUSES.Draft.toLowerCase()) {
+      this.intervention.status === CONSTANTS.STATUSES.Draft.toLowerCase()) {
       setTimeout(() => {
         // delay micro task execution; set to make sure _signedDocChangedForDraft will run on page load
         if (signedDocument) {
