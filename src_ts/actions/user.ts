ea66ed0fa437b7ec81a38ea1957af5433d0dfb8d
lajos <kovago.lajos91@gmail.com>
2020-08-21 18:39:54 +0300

    removed unnecessary class

--- src_ts/actions/user.ts
+++ src_ts/actions/user.ts
@@ -1,14 +1,16 @@
+//this to eb removed!!!!!!!!!!!!!
+//use simple User model flrom global types
+
 import {Action, ActionCreator} from 'redux';
-import {EtoolsUserModel} from '../components/user/user-model';
 // import {ThunkAction} from 'redux-thunk';
 // import {RootState} from '../store';
-import {AnyObject} from '../typings/globals.types';
+import {AnyObject, User} from '../typings/globals.types';
 
 export const UPDATE_USER_DATA = 'UPDATE_USER_DATA';
 export const UPDATE_USER_PERMISSIONS = 'UPDATE_USER_PERMISSIONS';
 
 export interface UserActionUpdate extends Action<'UPDATE_USER_DATA'> {
-  data: EtoolsUserModel;
+  data: User;
 }
 export interface UserActionUpdatePermissions extends Action<'UPDATE_USER_PERMISSIONS'> {
   permissions: AnyObject;
@@ -18,7 +20,7 @@ export type UserAction = UserActionUpdate | UserActionUpdatePermissions;
 // @ts-ignore - for now
 // type ThunkResult = ThunkAction<void, RootState, undefined, UserAction>;
 
-export const updateUserData: ActionCreator<UserActionUpdate> = (data: EtoolsUserModel) => {
+export const updateUserData: ActionCreator<UserActionUpdate> = (data: User) => {
   return {
     type: UPDATE_USER_DATA,
     data
