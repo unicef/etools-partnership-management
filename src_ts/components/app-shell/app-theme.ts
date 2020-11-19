9739b94cfcd7c198aa02f00cd5c19ac3fb6ce4b1
Adriana Trif <trif.corina.adriana@gmail.com>
2020-11-19 20:35:13 +0200

    Update app-theme.ts

--- src_ts/components/app-shell/app-theme.ts
+++ src_ts/components/app-shell/app-theme.ts
@@ -102,7 +102,7 @@ documentContainer.innerHTML = `
         --etools-action-button-main-color: var(--primary-color);
         --etools-action-button-text-color: var(--light-primary-text-color);
         --etools-action-button-dropdown-higlight-bg: var(--list-second-bg-color);
-        --etools-action-button-divider-color: #54a6fe;
+        --etools-action-button-divider-color: rgba(139,0,216, .99); /* .99 is to fix chrome strange bg color transparency */
 
         /* etools-status styles */
         --etools-status-divider-color: var(--light-divider-color);
