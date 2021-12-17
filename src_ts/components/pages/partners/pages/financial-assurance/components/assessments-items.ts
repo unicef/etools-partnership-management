50bc7818856c396e02c3a1b21912d5b32ec675a7
dcioara <dan.cioara@nordlogic.com>
2021-12-17 16:07:28 +0200

    [ch27875] Partner Details - Assurance - overview, activities - migrate to LitElement

--- src_ts/components/pages/partners/pages/financial-assurance/components/assessments-items.ts
+++ src_ts/components/pages/partners/pages/financial-assurance/components/assessments-items.ts
@@ -1,5 +1,5 @@
 /* eslint-disable lit-a11y/anchor-is-valid */
-import {PolymerElement, html} from '@polymer/polymer';
+import {LitElement, html, customElement, property} from 'lit-element';
 import '@polymer/iron-icons/iron-icons.js';
 import '@polymer/paper-icon-button/paper-icon-button.js';
 import '@unicef-polymer/etools-content-panel/etools-content-panel.js';
@@ -9,17 +9,17 @@ import '@unicef-polymer/etools-data-table/etools-data-table.js';
 import '../../../../../endpoints/endpoints.js';
 import CommonMixin from '../../../../../common/mixins/common-mixin.js';
 
-import {gridLayoutStyles} from '../../../../../styles/grid-layout-styles';
-import {SharedStyles} from '../../../../../styles/shared-styles';
+import {gridLayoutStylesLit} from '@unicef-polymer/etools-modules-common/dist/styles/grid-layout-styles-lit';
+import {sharedStyles} from '../../../../../styles/shared-styles-lit';
 import '../../../../../common/components/icons-actions';
 import './assessment-dialog.js';
-import {etoolsCpHeaderActionsBarStyles} from '../../../../../styles/etools-cp-header-actions-bar-styles';
+import {etoolsCpHeaderActionsBarStyles} from '../../../../../styles/etools-cp-header-actions-bar-styles-lit';
 import {store} from '../../../../../../redux/store';
 import {DECREASE_UPLOADS_IN_PROGRESS, INCREASE_UNSAVED_UPLOADS} from '../../../../../../redux/actions/upload-status';
-import {property} from '@polymer/decorators';
 import {PartnerAssessment} from '../../../../../../models/partners.models';
 import {fireEvent} from '../../../../../utils/fire-custom-event';
 import {openDialog} from '../../../../../utils/dialog';
+import {translate} from 'lit-translate';
 
 /**
  * @customElement
@@ -27,11 +27,15 @@ import {openDialog} from '../../../../../utils/dialog';
  * @mixinFunction
  * @appliesMixin CommonMixin
  */
-class AssessmentsItems extends CommonMixin(PolymerElement) {
-  static get template() {
+@customElement('assessments-items')
+export class AssessmentsItems extends CommonMixin(LitElement) {
+  static get styles() {
+    return [gridLayoutStylesLit];
+  }
+  render() {
     // language=HTML
     return html`
-      ${gridLayoutStyles} ${SharedStyles} ${etoolsCpHeaderActionsBarStyles}
+      ${sharedStyles} ${etoolsCpHeaderActionsBarStyles}
       <style include="data-table-styles">
         [hidden] {
           display: none !important;
@@ -70,19 +74,19 @@ class AssessmentsItems extends CommonMixin(PolymerElement) {
       </style>
 
       <etools-content-panel
-        panel-title="[[_getTranslation('OTHER_ASSESSMENTS')]] ([[dataItems.length]])"
+        panel-title="${translate('OTHER_ASSESSMENTS')} (${this.dataItems.length})"
         class="content-section"
       >
         <div slot="panel-btns" class="cp-header-actions-bar">
           <paper-toggle-button id="showArchived" checked="{{showArchived}}">
-            [[_getTranslation('SHOW_ARCHIVED')]]
+            ${translate('SHOW_ARCHIVED')}
           </paper-toggle-button>
           <div class="separator" hidden$="[[!editMode]]"></div>
           <paper-icon-button
             icon="add-box"
             disabled="[[!editMode]]"
             hidden$="[[!editMode]]"
-            title="[[_getTranslation('ADD_OTHER_ASSESSMENT')]]"
+            title="${translate('ADD_OTHER_ASSESSMENT')}"
             on-tap="_addAssessment"
           >
           </paper-icon-button>
@@ -90,52 +94,50 @@ class AssessmentsItems extends CommonMixin(PolymerElement) {
 
         <div hidden$="[[_emptyList(dataItems.length)]]">
           <etools-data-table-header no-collapse no-title>
-            <etools-data-table-column class="col-3"> [[_getTranslation('ASSESSMENT_TYPE')]] </etools-data-table-column>
-            <etools-data-table-column class="col-2">
-              [[_getTranslation('DATE_OF_ASSESSMENT')]]
-            </etools-data-table-column>
-            <etools-data-table-column class="col-5"> [[_getTranslation('REPORT')]] </etools-data-table-column>
-            <etools-data-table-column class="col-2 center-align">
-              [[_getTranslation('ARCHIVED')]]
-            </etools-data-table-column>
+            <etools-data-table-column class="col-3"> ${translate('ASSESSMENT_TYPE')} </etools-data-table-column>
+            <etools-data-table-column class="col-2"> ${translate('DATE_OF_ASSESSMENT')} </etools-data-table-column>
+            <etools-data-table-column class="col-5"> ${translate('REPORT')} </etools-data-table-column>
+            <etools-data-table-column class="col-2 center-align"> ${translate('ARCHIVED')} </etools-data-table-column>
           </etools-data-table-header>
 
-          <template is="dom-repeat" items="{{dataItems}}">
-            <etools-data-table-row
-              secondary-bg-on-hover
-              no-collapse
-              hidden$="[[!_isVisible(item.active, showArchived)]]"
-            >
-              <div slot="row-data" class="p-relative">
-                <span class="col-data col-3"> [[item.type]] </span>
-                <span class="col-data col-2"> [[getDateDisplayValue(item.completed_date)]] </span>
-                <span class="col-data col-5">
-                  <iron-icon icon="attachment" class="attachment"></iron-icon>
-                  <span class="break-word">
-                    <!-- target="_blank" is there for IE -->
-                    <a href$="[[item.report_attachment]]" target="_blank" download>
-                      [[getFileNameFromURL(item.report_attachment)]]
-                    </a>
+          ${this.dataItems.map(
+            (item) => html`
+              <etools-data-table-row
+                secondary-bg-on-hover
+                no-collapse
+                ?hidden="${!this._isVisible(item.active, this.showArchived)}"
+              >
+                <div slot="row-data" class="p-relative">
+                  <span class="col-data col-3"> ${item.type} </span>
+                  <span class="col-data col-2"> ${this.getDateDisplayValue(item.completed_date)} </span>
+                  <span class="col-data col-5">
+                    <iron-icon icon="attachment" class="attachment"></iron-icon>
+                    <span class="break-word">
+                      <!-- target="_blank" is there for IE -->
+                      <a href="${item.report_attachment}" target="_blank" download>
+                        ${this.getFileNameFromURL(item.report_attachment)}
+                      </a>
+                    </span>
                   </span>
-                </span>
-                <span class="col-data col-2 center-align">
-                  <span hidden$="[[!item.active]]" class="placeholder-style">&#8212;</span>
-                  <iron-icon icon="check" hidden$="[[item.active]]"></iron-icon>
-                </span>
-                <icons-actions2
-                  item-id$="[[item.id]]"
-                  hidden$="[[!editMode]]"
-                  on-edit="_editAssessment"
-                  show-delete="[[showDelete]]"
-                >
-                </icons-actions2>
-              </div>
-            </etools-data-table-row>
-          </template>
+                  <span class="col-data col-2 center-align">
+                    <span ?hidden="${!item.active}" class="placeholder-style">&#8212;</span>
+                    <iron-icon icon="check" ?hidden="${item.active}"></iron-icon>
+                  </span>
+                  <icons-actions2
+                    .itemId="${item.id}"
+                    ?hidden="${!this.editMode}"
+                    @edit="${this._editAssessment}"
+                    .showDelete="${this.showDelete}"
+                  >
+                  </icons-actions2>
+                </div>
+              </etools-data-table-row>
+            `
+          )}
         </div>
 
-        <div class="row-h no-assessments-warning" hidden$="[[!_emptyList(dataItems.length)]]">
-          <p>[[_getTranslation('THERE_ARE_NO_ASSESSMENTS_ADDED')]]</p>
+        <div class="row-h no-assessments-warning" ?hidden="${!this._emptyList(this.dataItems.length)}">
+          <p>${translate('THERE_ARE_NO_ASSESSMENTS_ADDED')}</p>
         </div>
       </etools-content-panel>
     `;
@@ -147,15 +149,20 @@ class AssessmentsItems extends CommonMixin(PolymerElement) {
   @property({type: Number})
   partnerId: number | null = null;
 
-  @property({type: Boolean, reflectToAttribute: true})
+  @property({type: Boolean, reflect: true})
   open = true;
 
-  @property({
-    type: Boolean,
-    reflectToAttribute: true,
-    observer: '_editModeChanged'
-  })
-  editMode!: boolean;
+  _editMode!: boolean;
+
+  set editMode(editMode) {
+    this._editMode = editMode;
+    // this._editModeChanged(this._editMode);
+  }
+
+  @property({type: Boolean, reflect: true})
+  get editMode() {
+    return this._editMode;
+  }
 
   @property({type: Boolean})
   showArchived = false;
@@ -164,7 +171,7 @@ class AssessmentsItems extends CommonMixin(PolymerElement) {
   showDelete = false;
 
   newAssessmentAdded(data: any) {
-    this.push('dataItems', data.detail);
+    this.dataItems.push(data.detail);
     fireEvent(this, 'assessment-added-step2', data.detail);
   }
 
@@ -175,7 +182,7 @@ class AssessmentsItems extends CommonMixin(PolymerElement) {
     if (idx > -1) {
       assessments.splice(idx, 1, updatedAss);
     }
-    this.set('dataItems', assessments);
+    this.dataItems = assessments;
 
     fireEvent(this, 'assessment-updated-step2', data.detail);
   }
@@ -217,7 +224,8 @@ class AssessmentsItems extends CommonMixin(PolymerElement) {
     if (e.detail.success) {
       const assessmentIndex = Number((e.target as any).getAttribute('data-args-index')); // TODO - who is e.target
       const uploadResponse = JSON.parse(e.detail.success);
-      this.set(['dataItems', assessmentIndex, 'report_attachment'], uploadResponse.id);
+      // @dci this.set(['dataItems', assessmentIndex, 'report_attachment'], uploadResponse.id);
+      this.dataItems[assessmentIndex] = uploadResponse.id;
       store.dispatch({type: INCREASE_UNSAVED_UPLOADS});
     }
   }
@@ -230,5 +238,3 @@ class AssessmentsItems extends CommonMixin(PolymerElement) {
     return length === 0;
   }
 }
-
-window.customElements.define('assessments-items', AssessmentsItems);
