1b940734750c0ef85b96ffa23504dc55f0037073
merceaemil <30448376+merceaemil@users.noreply.github.com>
2022-02-02 15:57:53 +0200

    [ch28256] Agreements: Cannot sign Agreement (#1777)
    Co-authored-by: Mercea Emil <office@insightfeatures.com>

--- src_ts/components/pages/agreements/pages/details/agreement-details.ts
+++ src_ts/components/pages/agreements/pages/details/agreement-details.ts
@@ -195,10 +195,13 @@ class AgreementDetails extends connect(store)(CommonMixin(UploadsMixin(StaffMemb
               <datepicker-lite
                 id="startDateField"
                 label="[[_getTranslation('START_DATE')]]"
-                value="{{agreement.start}}"
+                value="[[agreement.start]]"
                 readonly$="[[!agreement.permissions.edit.start]]"
                 required$="[[agreement.permissions.required.start]]"
                 selected-date-display-format="D MMM YYYY"
+                fire-date-has-changed
+                on-date-has-changed="_dateHasChanged"
+                data-field-path="agreement.start"
               >
               </datepicker-lite>
             </div>
@@ -206,10 +209,13 @@ class AgreementDetails extends connect(store)(CommonMixin(UploadsMixin(StaffMemb
               <datepicker-lite
                 id="endDateField"
                 label="[[_getTranslation('END_DATE')]]"
-                value="{{agreement.end}}"
+                value="[[agreement.end]]"
                 readonly$="[[!agreement.permissions.edit.end]]"
                 required$="[[agreement.permissions.required.end]]"
                 selected-date-display-format="D MMM YYYY"
+                fire-date-has-changed
+                on-date-has-changed="_dateHasChanged"
+                data-field-path="agreement.end"
               >
               </datepicker-lite>
             </div>
@@ -256,10 +262,13 @@ class AgreementDetails extends connect(store)(CommonMixin(UploadsMixin(StaffMemb
               <datepicker-lite
                 id="signedByPartnerDateField"
                 label="[[_getTranslation('SIGNED_BY_PARTNER_DATE')]]"
-                value="{{agreement.signed_by_partner_date}}"
+                value="[[agreement.signed_by_partner_date]]"
                 readonly$="[[!agreement.permissions.edit.signed_by_partner_date]]"
                 max-date="[[getCurrentDate()]]"
                 selected-date-display-format="D MMM YYYY"
+                fire-date-has-changed
+                on-date-has-changed="_dateHasChanged"
+                data-field-path="agreement.signed_by_partner_date"
               >
               </datepicker-lite>
             </div>
@@ -276,10 +285,13 @@ class AgreementDetails extends connect(store)(CommonMixin(UploadsMixin(StaffMemb
               <datepicker-lite
                 id="signedByUnicefDateField"
                 label="[[_getTranslation('SIGNED_BY_UNICEF_DATE')]]"
-                value="{{agreement.signed_by_unicef_date}}"
+                value="[[agreement.signed_by_unicef_date]]"
                 readonly$="[[!agreement.permissions.edit.signed_by_unicef_date]]"
                 max-date="[[getCurrentDate()]]"
                 selected-date-display-format="D MMM YYYY"
+                fire-date-has-changed
+                on-date-has-changed="_dateHasChanged"
+                data-field-path="agreement.signed_by_unicef_date"
               >
               </datepicker-lite>
             </div>
