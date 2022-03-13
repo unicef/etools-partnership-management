cb60156c48b6559d22b232459f05b9443e4f4c3a
Trif Adriana <trif.corina.adriana@gmail.com>
2022-03-13 17:45:46 +0200

    migrate components

--- src_ts/components/pages/not-found/not-found.ts
+++ src_ts/components/pages/not-found/not-found.ts
@@ -1,16 +1,21 @@
-import {PolymerElement, html} from '@polymer/polymer';
-import '@polymer/paper-styles/element-styles/paper-material-styles';
-import {SharedStyles} from '../../styles/shared-styles';
+import {elevationStyles} from '@unicef-polymer/etools-modules-common/dist/styles/elevation-styles';
+import {sharedStyles} from '@unicef-polymer/etools-modules-common/dist/styles/shared-styles-lit';
+import {html, LitElement} from 'lit-element';
+import {BASE_URL} from '../../../config/config';
+import {pageLayoutStyles} from '../../styles/page-layout-styles-lit';
 
 /**
  * @polymer
  * @customElement
  */
-class NotFound extends PolymerElement {
-  static get template() {
+class NotFound extends LitElement {
+  static get styles() {
+    return [elevationStyles];
+  }
+  render() {
     return html`
-      ${SharedStyles}
-      <style include="paper-material-styles">
+      ${sharedStyles}${pageLayoutStyles}
+      <style>
         :host {
           display: block;
           width: 100%;
@@ -34,11 +39,16 @@ class NotFound extends PolymerElement {
           @apply --page-title;
           margin-bottom: 24px;
         }
+
+        .page-content {
+          background-color: var(--primary-background-color);
+          padding: 18px 24px;
+        }
       </style>
 
-      <div class="paper-material" elevation="1">
+      <div class="page-content elevation" elevation="1">
         <h1>404 - Page not found!</h1>
-        <a href$="[[rootPath]]partners/list">Head back home.</a>
+        <a href="${BASE_URL}partners/list">Head back home.</a>
       </div>
     `;
   }
