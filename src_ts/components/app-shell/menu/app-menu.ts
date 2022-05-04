21f00985b27c424366709407c636f6cbb363185e
Adriana Trif <trif.corina.adriana@gmail.com>
2022-05-04 18:45:51 +0300

    Implem. Intell. link (#1849)

--- src_ts/components/app-shell/menu/app-menu.ts
+++ src_ts/components/app-shell/menu/app-menu.ts
@@ -121,7 +121,7 @@ class AppMenu extends connect(store)(
 
         <a
           class="nav-menu-item lighter-item no-transform"
-          href="https://app.powerbi.com/groups/me/apps/2c83563f-d6fc-4ade-9c10-bbca57ed1ece/reports/f9497d74-a670-4051-8800-a9d82284954f/ReportSection"
+          href="https://app.powerbi.com/groups/me/apps/2c83563f-d6fc-4ade-9c10-bbca57ed1ece/reports/9726e9e7-c72f-4153-9fd2-7b418a1e426c/ReportSection?ctid=77410195-14e1-4fb8-904b-ab1892023667"
           target="_blank"
         >
           <iron-icon id="power-bi-icon" icon="pmp-main-icons:power-bi"></iron-icon>
