310b1688e94909ac17495e13e73d8cdce6f849dc
Uladzimir Urbanovich <v_urbanovich@mail.ru>
2022-05-26 16:50:55 +0300

    [ch30016] Fix page size change on reports page (#1885)

--- src_ts/components/pages/reports/pages/list/reports-list.ts
+++ src_ts/components/pages/reports/pages/list/reports-list.ts
@@ -439,7 +439,7 @@ export class ReportsList extends connect(store)(PaginationMixin(CommonMixin(Endp
       unicef_focal_points: queryParams.unicef_focal_points,
       report_type: queryParams.report_type,
       page: queryParams.page ? Number(queryParams.page) : 1,
-      page_size: queryParams.page_size ? Number(queryParams.page_size) : CONSTANTS.DEFAULT_LIST_SIZE
+      page_size: queryParams.size ? Number(queryParams.size) : CONSTANTS.DEFAULT_LIST_SIZE
     };
     Object.keys(filterValues).forEach((key) => {
       if (!filterValues[key]) {
