5f47a53cb90a575076ee18ddb62e108b88ad82e2
Trif Adriana <trif.corina.adriana@gmail.com>
2022-11-14 20:46:54 +0200

    Use static imports for lazy loaded pages

--- src_ts/redux/actions/app.ts
+++ src_ts/redux/actions/app.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /**
 @license
 Copyright (c) 2018 The Polymer Project Authors. All rights reserved.
@@ -11,6 +12,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 import {Action, ActionCreator} from 'redux';
 import {UPDATE_ROUTE_DETAILS} from './actionsConstants';
 import {RouteDetails} from '../../components/utils/router';
+import {EtoolsRouter} from '../../components/utils/routes';
 export const UPDATE_DRAWER_STATE = 'UPDATE_DRAWER_STATE';
 export const UPDATE_SMALLMENU_STATE = 'UPDATE_SMALLMENU_STATE';
 export const RESET_CURRENT_ITEM = 'RESET_CURRENT_ITEM';
@@ -48,7 +50,121 @@ export const updateSmallMenu: any = (smallMenu: boolean) => {
   };
 };
 
+const loadPageComponents = (routeDetails: RouteDetails) => (dispatch, getState) => {
+  if (!routeDetails) {
+    // invalid route => redirect to 404 page
+    EtoolsRouter.updateAppLocation('/not-found');
+    return;
+  }
+
+  if (routeDetails.routeName === 'not-found') {
+    import('../../components/pages/not-found/not-found.js');
+  } else {
+    if (routeDetails.routeName == 'interventions') {
+      switch (routeDetails.subRouteName) {
+        case 'list':
+          import('../../components/pages/interventions/pages/list/interventions-list');
+          break;
+        case 'metadata':
+          import('../../components/pages/interventions/pages/intervention-tab-pages/intervention-tabs.js');
+          import(
+            '../../components/pages/interventions/pages/intervention-tab-pages/intervention-metadata/intervention-metadata.js'
+          );
+          break;
+        case 'workplan':
+          import('../../components/pages/interventions/pages/intervention-tab-pages/intervention-tabs.js');
+          import(
+            '../../components/pages/interventions/pages/intervention-tab-pages/intervention-workplan/intervention-workplan.js'
+          );
+          break;
+        case 'workplan-editor':
+          import('../../components/pages/interventions/pages/intervention-tab-pages/intervention-tabs.js');
+          import(
+            '../../components/pages/interventions/pages/intervention-tab-pages/intervention-workplan-editor/intervention-workplan-editor.js'
+          );
+          break;
+        case 'timing':
+          import('../../components/pages/interventions/pages/intervention-tab-pages/intervention-tabs.js');
+          import(
+            '../../components/pages/interventions/pages/intervention-tab-pages/intervention-timing/intervention-timing.js'
+          );
+          break;
+        case 'strategy':
+          import('../../components/pages/interventions/pages/intervention-tab-pages/intervention-tabs.js');
+          import(
+            '../../components/pages/interventions/pages/intervention-tab-pages/intervention-strategy/intervention-strategy.js'
+          );
+          break;
+        case 'attachments':
+          import('../../components/pages/interventions/pages/intervention-tab-pages/intervention-tabs.js');
+          import(
+            '../../components/pages/interventions/pages/intervention-tab-pages/intervention-attachments/intervention-attachments.js'
+          );
+          break;
+        case 'review':
+          import('../../components/pages/interventions/pages/intervention-tab-pages/intervention-tabs.js');
+          import(
+            '../../components/pages/interventions/pages/intervention-tab-pages/intervention-review/intervention-review.js'
+          );
+          break;
+        case 'progress':
+          import('../../components/pages/interventions/pages/intervention-tab-pages/intervention-tabs.js');
+          import(
+            '../../components/pages/interventions/pages/intervention-tab-pages/intervention-progress/intervention-progress.js'
+          );
+          break;
+
+        default:
+          console.log('No file imports configuration found (componentsLazyLoadConfig)!');
+          EtoolsRouter.updateAppLocation('/not-found');
+          break;
+      }
+    } else if (routeDetails.routeName == 'partners') {
+      switch (routeDetails.subRouteName) {
+        case 'list':
+          import('../../components/pages/partners/pages/list/partners-list');
+          break;
+        case 'details':
+          import('../../components/pages/partners/pages/details/partner-details.js');
+          break;
+        case 'financial-assurance':
+          import(
+            '../../components/pages/interventions/pages/intervention-tab-pages/intervention-metadata/intervention-metadata.js'
+          );
+          break;
+        case 'overview':
+          import(
+            '../../components/pages/interventions/pages/intervention-tab-pages/intervention-metadata/intervention-metadata.js'
+          );
+          break;
+        default:
+          console.log('No file imports configuration found (componentsLazyLoadConfig)!');
+          EtoolsRouter.updateAppLocation('/not-found');
+          break;
+      }
+    } else if (routeDetails.routeName == 'governments') {
+      // TODO
+    } else if (routeDetails.routeName == 'agreements') {
+      switch (routeDetails.subRouteName) {
+        case 'list':
+          import('../../components/pages/interventions/pages/list/interventions-list');
+          break;
+        case 'details':
+          import(
+            '../../components/pages/interventions/pages/intervention-tab-pages/intervention-metadata/intervention-metadata.js'
+          );
+          break;
+        default:
+          console.log('No file imports configuration found (componentsLazyLoadConfig)!');
+          EtoolsRouter.updateAppLocation('/not-found');
+          break;
+      }
+    }
+  }
+};
+
 export const updateStoreRouteDetails: ActionCreator<AppActionUpdateRouteDetails> = (routeDetails: any) => {
+  loadPageComponents(routeDetails);
   return {
     type: UPDATE_ROUTE_DETAILS,
     routeDetails
