0f3d4af73516b0657c2f830f48bea1904e8da0f0
Trif Adriana <trif.corina.adriana@gmail.com>
2022-11-17 21:43:39 +0200

    Try polymer custom build with Babel/plugin-proposal-nullish-coalescing-operator

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+const gulp = require('gulp');
+const mergeStream = require('merge-stream');
+const babel = require('gulp-babel');
+const swConfig = require('./sw-precache-config');
+const polymerBuild = require('polymer-build');
+const fork = polymerBuild.forkStream;
+const addServiceWorker = polymerBuild.addServiceWorker;
+const PolymerProject = polymerBuild.PolymerProject;
+
+const project = new PolymerProject(require('./polymer.json'));
+
+function waitFor(stream) {
+  return new Promise((resolve, reject) => {
+    stream.on('end', resolve);
+    stream.on('error', reject);
+  });
+}
+
+// Create a build pipeline to pipe both streams together to the 'build/' dir
+const customBuild = () => {
+  // merge the source and dependencies streams to we can analyze the project
+  const allFiles = mergeStream(project.sources(), project.dependencies()).pipe(project.analyzer);
+
+  // fork the stream in case downstream transformers mutate the files
+  // this fork will generate the bundle files for the project
+  const bundledPhase = fork(allFiles)
+    .pipe(project.bundler())
+    .pipe(
+      babel({
+        plugins: ['@babel/plugin-proposal-nullish-coalescing-operator']
+      })
+    )
+    // write to the bundled folder
+    // TODO(justinfagnani): allow filtering of files before writing
+    .pipe(gulp.dest('build/esm-bundled'));
+
+  // Once the bundled build stream is complete, create a service worker for the
+  // build
+  const bundledPostProcessing = waitFor(bundledPhase).then(() => {
+    return addServiceWorker({
+      project: project,
+      buildRoot: 'build/esm-bundled',
+      swConfig: swConfig,
+      bundled: true
+    });
+  });
+
+  return Promise.all([bundledPostProcessing]);
+};
+
+gulp.task('default', customBuild);
