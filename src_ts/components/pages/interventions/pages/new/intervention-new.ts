284c5cd106f5c5455793b4202c0c6e50a414c65e
Mercea Emil <office@insightfeatures.com>
2023-01-11 17:07:14 +0200

    [ch32690] Display users as inactive if they are marked as inactive

--- src_ts/components/pages/interventions/pages/new/intervention-new.ts
+++ src_ts/components/pages/interventions/pages/new/intervention-new.ts
@@ -129,10 +129,15 @@ export class InterventionNew extends connect(store)(LitElement) {
     this.newIntervention.partner_focal_points = [];
     sendRequest({endpoint}).then(
       (users: PartnerStaffMember[]) =>
-        (this.staffMembers = users.map((member: PartnerStaffMember) => ({
-          label: `${member.first_name} ${member.last_name}`,
-          value: member.id
-        })))
+        (this.staffMembers = users
+          .sort((a: PartnerStaffMember, b: PartnerStaffMember) =>  
+            Number(b.active) - Number(a.active) || `${a.first_name} ${a.last_name}`.localeCompare(`${b.first_name} ${b.last_name}`)
+          )
+          .map((member: PartnerStaffMember) => ({
+            label: `${!member.active ? '[Inactive]' : ''} ${member.first_name} ${member.last_name} (${member.email})`,
+            value: member.id
+          }))
+          
     );
   }
 
