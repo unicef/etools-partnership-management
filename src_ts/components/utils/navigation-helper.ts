3d507bec84171f2387ddf29ee1c3bd56a9a8ebd9
Trif Adriana <trif.corina.adriana@gmail.com>
2023-03-03 12:58:19 +0200

    Routing clean up

--- src_ts/components/utils/navigation-helper.ts
+++ src_ts/components/utils/navigation-helper.ts
@@ -1,6 +1,3 @@
-import { handleUrlChange } from "../../redux/actions/app";
-import { store } from "../../redux/store";
-
 /**
  * Update app state
  */
@@ -11,10 +8,7 @@ export function replaceAppState(routePath: string, qs: string, dispatchLocationC
   window.history.replaceState(currentState, '', routePath + (qs.length ? '?' + qs : ''));
 
   if (dispatchLocationChange) {
-    // This event lets app-location and app-route know
-    // the URL has changed
-    window.dispatchEvent(new CustomEvent('location-changed'));
-    store.dispatch(handleUrlChange(routePath));
+    window.dispatchEvent(new CustomEvent('popstate'));
   }
 }
 
@@ -26,6 +20,5 @@ export function changeAppState(url: string) {
     return;
   }
   window.history.pushState(window.history.state, '', url);
-  window.dispatchEvent(new CustomEvent('location-changed'));
-  store.dispatch(handleUrlChange(url));
+  window.dispatchEvent(new CustomEvent('popstate'));
 }
