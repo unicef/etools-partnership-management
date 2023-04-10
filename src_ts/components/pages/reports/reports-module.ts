4cc745b73fb6fa15968e1808541610cd4499a2ba
Mercea Emil <office@insightfeatures.com>
2023-04-10 14:32:36 +0300

    [ch33131] [ch33272] [ch33140] Fixes

--- src_ts/components/pages/reports/reports-module.ts
+++ src_ts/components/pages/reports/reports-module.ts
@@ -126,7 +126,7 @@ export class ReportsModule extends connect(store)(
 
         <div slot="title-row-actions" class="content-header-actions move-to-the-right">
           <div class="action" ?hidden="${!this.listActive}">
-            <paper-menu-button id="export" close-on-activate horizontal-align="right">
+            <paper-menu-button id="export" close-on-activate horizontal-align>
               <paper-button slot="dropdown-trigger" class="focus-as-link">
                 <iron-icon icon="file-download"></iron-icon>
                 ${translate('EXPORT')}
@@ -162,7 +162,7 @@ export class ReportsModule extends connect(store)(
               </paper-listbox>
             </paper-menu-button>
 
-            <paper-menu-button close-on-activate horizontal-align="right">
+            <paper-menu-button close-on-activate horizontal-align>
               <paper-button slot="dropdown-trigger" class="dropdown-trigger">
                 <iron-icon icon="more-vert"></iron-icon>
               </paper-button>
