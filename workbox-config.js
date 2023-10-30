0b22108583eb47ca670bf5e6e9da9cf32b2105ca
Mercea Emil <office@insightfeatures.com>
2023-10-30 11:41:01 +0200

    Rollup update + serviceworker + submodule ref

--- workbox-config.js
+++ workbox-config.js
@@ -2,22 +2,41 @@ export const workboxConfig = {
   globDirectory: "src/",
   globPatterns: [
     'manifest.json',
-    '**/*'
+    'src/**/*',
+    'node_modules/**/*',
+    'assets/**/*',
+    'images/**/*'
   ],
   swDest: "src/service-worker.js",
+  exclude: ['version.json'],
   runtimeCaching: [
     {
       urlPattern: /\/@webcomponents\/webcomponentsjs\//,
       handler: 'StaleWhileRevalidate'
     },
     {
-      urlPattern: /^https:\/\/fonts.gstatic.com\//,
-      handler: 'StaleWhileRevalidate'
+      urlPattern: /^https:\/\/fonts\.googleapis\.com/,
+      handler: "StaleWhileRevalidate",
+      options: {
+        cacheName: "google-fonts-stylesheets",
+        cacheableResponse: {
+          statuses: [0, 200]
+        }
+      }
     },
     {
-      urlPattern: /^https:\/\/fonts.googleapis.com\//,
-      handler: 'StaleWhileRevalidate'
+      urlPattern: /^https:\/\/fonts\.gstatic\.com/,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "google-fonts-webfonts",
+        cacheableResponse: {
+          statuses: [0, 200]
+        },
+        expiration: {
+          maxAgeSeconds: 60 * 60 * 24 * 365,
+          maxEntries: 30
+        }
+      }
     }
-
   ]
 };
