04b7e2f350fba9ba358caeada6c64f0b3a271b1c
Mercea Emil <office@insightfeatures.com>
2023-11-03 15:07:13 +0200

    Test build circlci with revision and cache changes

--- src_ts/app-shell.ts
+++ src_ts/app-shell.ts
@@ -459,6 +459,8 @@ class AppShell extends connect(store)(
           console.log('version.json', version.revision);
           console.log('buildRevNo ', document.getElementById('buildRevNo')!.innerText);
           this._showConfirmNewVersionDialog();
+        } else {
+          console.log('Version did not change: ', version.revision, document.getElementById('buildRevNo')!.innerText);
         }
       });
   }
--- workbox-config.js
+++ workbox-config.js
@@ -2,6 +2,7 @@ export const workboxConfig = {
   globDirectory: "src/",
   globPatterns: [
     'manifest.json',
+    'version.json',
     'src/**/*',
     'node_modules/**/*',
     'assets/**/*',
@@ -14,28 +15,8 @@ export const workboxConfig = {
       handler: 'StaleWhileRevalidate'
     },
     {
-      urlPattern: /^https:\/\/fonts\.googleapis\.com/,
-      handler: "StaleWhileRevalidate",
-      options: {
-        cacheName: "google-fonts-stylesheets",
-        cacheableResponse: {
-          statuses: [0, 200]
-        }
-      }
-    },
-    {
       urlPattern: /^https:\/\/fonts\.gstatic\.com/,
-      handler: "CacheFirst",
-      options: {
-        cacheName: "google-fonts-webfonts",
-        cacheableResponse: {
-          statuses: [0, 200]
-        },
-        expiration: {
-          maxAgeSeconds: 60 * 60 * 24 * 365,
-          maxEntries: 30
-        }
-      }
+      handler: 'StaleWhileRevalidate'
     }
   ]
 };
