a935f2b4dea0503ce3b7f06d18264bcb183dc368
Pendea Raul <raul.pendea@nordlogic.com>
2024-08-21 13:04:41 +0300

    [ch35190] added accepted_comment input

--- src_ts/components/pages/reports/components/report-rating-dialog.ts
+++ src_ts/components/pages/reports/components/report-rating-dialog.ts
@@ -62,18 +62,16 @@ export class ReportRatingDialog extends connect(store)(EndpointsLitMixin(LitElem
             <sl-radio value="NoP"> ${translate('NO_PROGRESS')}</sl-radio>
             <sl-radio value="Con"> ${translate('CONSTRAINED')}</sl-radio>
           </etools-radio-group>
-          <div>
-            <etools-input
-              id="comment"
-              label="${translate('COMMENT')}"
-              .value="${this.acceptedComment}"
-              @value-changed="${({detail}: CustomEvent) => {
-                this.acceptedComment = detail.value;
-              }}"
-              placeholder="&#8212;"
-              maxlength="50"
-            ></etools-input>
-          </div>
+          <etools-input
+            id="comment"
+            label="${translate('COMMENT')}"
+            .value="${this.acceptedComment}"
+            @value-changed="${({detail}: CustomEvent) => {
+              this.acceptedComment = detail.value;
+            }}"
+            placeholder="&#8212;"
+            maxlength="50"
+          ></etools-input>
         </div>
       </etools-dialog>
     `;
