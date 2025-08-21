1dce5831f42a3148b19b5dccdfc5074c2f7240ef
Pendea Raul <raul.pendea@nordlogic.com>
2025-08-21 11:45:56 +0300

    [271856] Removed Government Link Left a TODO to remove code related for this if everything is ok

--- src_ts/components/app-shell/menu/app-menu.ts
+++ src_ts/components/app-shell/menu/app-menu.ts
@@ -107,7 +107,8 @@ class AppMenu extends connect(store)(MatomoMixin(EnvironmentFlagsMixin(LitElemen
 
             <div class="name">${translate('GPD')}</div>
           </a>
-          <a
+          <!-- TODO remove routes for this page and code related only
+            <a
             class="nav-menu-item ${this.getItemClass(this.selectedOption, 'government-partners')}"
             menu-name="government-partners"
             href="${Environment.basePath}government-partners/list"
@@ -115,8 +116,8 @@ class AppMenu extends connect(store)(MatomoMixin(EnvironmentFlagsMixin(LitElemen
             <sl-tooltip placement="right" ?disabled="${!this.smallMenu}" content="${translate('GOVERNMENT')}">
               <etools-icon id="gov-icon" name="account-balance"></etools-icon>
             </sl-tooltip>
-            <div class="name">${translate('GOVERNMENT')}</div>
-          </a>
+            <div class="name">${translate('GOVERNMENT')}</div> </a
+          > -->
 
           <a
             class="nav-menu-item ${this.getItemClass(this.selectedOption, 'reports')}"
